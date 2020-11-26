a176c7827e6b5f6677213c2f9eef2c19fc681576
FUJITSU <deanumainah124@gmail.com>
2020-11-26 10:05:41 +0700

    ini

--- app/src/main/java/com/dean/retrofitcrud/remote/ProductServices.java
+++ app/src/main/java/com/dean/retrofitcrud/remote/ProductServices.java
@@ -12,28 +12,30 @@ import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.HTTP;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ProductServices {
 
-    @GET("person/get")
+    @GET("person/get/")
     Call<List<PersonItem>> getProduct();
 
     @FormUrlEncoded
-    @POST("person/add")
+    @POST("person/add/")
     Call<PersonItem> addProduct(@Field("name") String name,
                                 @Field("price") String price,
                                 @Field("desc") String desc);
 
     @FormUrlEncoded
-    @PUT("person/update")
+    @PUT("person/update/")
     Call<PersonItem> updateProduct(@Field("id") int id,
                                    @Field("name") String name,
                                    @Field("price") String price,
                                    @Field("desc") String desc);
 
-    @DELETE("person/delete")
-    Call<PersonItem> deleteProduct(@Path("id") int id);
+    @FormUrlEncoded
+    @HTTP(method = "DELETE", path = ("person/delete/"), hasBody = true)
+    Call<PersonItem> deleteProduct(@Field("id") int id);
 }
